81368cce81a23ec3020b0d09f394921ba487ff2d
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-11-23 14:14:00 -0500

    Fix test failure caused by consul-client 1.9.0 upgrade
    The Consul.Builder#build method no longer eagerly pings Consul
    when called. So, we can't force a failure anymore just by telling
    it to connect to an open port where nothing is running.
    
    Updated the existing test to validate the new behavior and added
    a new test to validate the original behavior (which should not
    happen anymore, but the test forces it).

--- src/test/java/org/kiwiproject/dropwizard/consul/ConsulBundleTest.java
+++ src/test/java/org/kiwiproject/dropwizard/consul/ConsulBundleTest.java
@@ -2,20 +2,26 @@ package org.kiwiproject.dropwizard.consul;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import io.dropwizard.configuration.ConfigurationSourceProvider;
 import io.dropwizard.core.Configuration;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.kiwiproject.consul.ConsulException;
 import org.kiwiproject.consul.config.CacheConfig;
 import org.kiwiproject.consul.config.ClientConfig;
 import org.kiwiproject.net.LocalPortChecker;
@@ -64,17 +70,47 @@ class ConsulBundleTest {
             assertThat(bundle.didInitializeSucceed()).isFalse();
         }
 
+        // As of consul-client 1.9.0, the Consul.Builder doesn't ping the
+        // Consul agent, so we should not see ConsulException anymore. Despite
+        // that, I kept this test but renamed it, since there should not be
+        // a ConsulException anymore.
         @Test
-        void shouldNotAllowConsulExceptionToEscape_WhenUnableToConnectToConsul() {
+        void shouldNotFail_WhenUnableToConnectToConsul() {
             var bootstrap = mock(Bootstrap.class);
+            when(bootstrap.getConfigurationSourceProvider()).thenReturn(mock(ConfigurationSourceProvider.class));
+
             var openPort = new LocalPortChecker().findRandomOpenPort().orElseThrow();
             doReturn(openPort).when(bundle).getConsulAgentPort();
             assertThatCode(() -> bundle.initialize(bootstrap)).doesNotThrowAnyException();
 
             assertThat(bundle.didAttemptInitialize()).isTrue();
+            assertThat(bundle.didInitializeSucceed()).isTrue();
+
+            verify(bootstrap).getConfigurationSourceProvider();
+            verify(bootstrap).setConfigurationSourceProvider(any());
+            verifyNoMoreInteractions(bootstrap);
+        }
+
+        // This test is to validate the original behavior to not allow
+        // ConsulException to "escape" if thrown. With the consul-client
+        // 1.9.0 change that no longer eagerly pings Consul, this should
+        // not happen anymore. Regardless, I added this test to validate
+        // the original behavior. That is the reason for introducing the
+        // buildConsulClient method, i.e., I needed to be able to cause
+        // the Consul.Builder#build method to throw a ConsulException.
+        @Test
+        void shouldNotAllowConsulExceptionToEscape_IfConsulExceptionThrown() {
+            var bootstrap = mock(Bootstrap.class);
+
+            doThrow(new ConsulException("unexpected error")).when(bundle).buildConsulClient(any());
+
+            assertThatCode(() -> bundle.initialize(bootstrap)).doesNotThrowAnyException();
+
+            assertThat(bundle.didAttemptInitialize()).isTrue();
             assertThat(bundle.didInitializeSucceed()).isFalse();
 
-            verifyNoInteractions(bootstrap);  // should throw before trying to set up the ConfigurationSourceProvider
+            verify(bundle).buildConsulClient(any());
+            verifyNoInteractions(bootstrap);
         }
     }
 
